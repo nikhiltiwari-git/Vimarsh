a9c2afe25a4167c73254ac3cb16aef9e5772aeb8
Nikhil Tiwari <nikhiltiwari1903@gmail.com>
2022-07-04 02:45:18 +0530

    price page commited chat page designed toggle not addes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Coin from "./components/coinitem/Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1 className="brand">
+           Vimarsh
+        </h1>
+        <form>
+          <input
+            className="inputField"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search..."
+          />
+        </form>
+      </div>
+      <div className="coinsContainer">
+      {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              volume={coin.total_volume}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/coinitem/Coin.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Coin.css";
+
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
+  return (
+    <div className="cryptoCoin">
+      <img src={image} alt={`${name}`} className="coinLogo" />
+      <div className="coinNameWrap">
+        <h1 className="coinName">{name}</h1>
+        <p className="coinSymbol">{symbol}</p>
+      </div>
+      <p className="coinPrice">${price.toLocaleString()}</p>
+      <p className="coinMarketcap">Market Cap: ${marketcap.toLocaleString()}</p>
+      <p className="coinVolume">Volume (24H): ${volume.toLocaleString()}</p>
+      {priceChange < 0 ? (
+        <div className="priceContainerDOWN">
+          <p className="priceChange">{priceChange.toFixed(2)}%</p>
+        </div>
+      ) : (
+        <div className="priceContainerUP">
+          <p className="priceChange">{priceChange.toFixed(2)}%</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Coin;
